b08c22de98f0e6f4304faba3b011f71b30103cbc
Brian Jordan <bcjordan@gmail.com>
2014-05-23 15:48:11 -0700

    Add test stub. Remove unused variable.

--- src/bounce/blocks.js
+++ src/bounce/blocks.js
@@ -24,7 +24,6 @@ var generateSetterCode = function (ctx, name) {
 
 // Install extensions to Blockly's language and JavaScript generator.
 exports.install = function(blockly, blockInstallOptions) {
-  var skin = blockInstallOptions.skin;
   var generator = blockly.Generator.get('JavaScript');
   blockly.JavaScript = generator;
 
